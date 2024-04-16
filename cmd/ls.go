95d39c81a9a0b733f7796703e07e671c10430a7d
Mathías Donoso <mathiasd88@gmail.com>
2024-04-15 23:11:35 -0400

    feat(ls): ls command shows pending and in_progress tasks by default (#8)

--- cmd/ls.go
+++ cmd/ls.go
@@ -31,9 +31,9 @@ showing the following information:
 	Run: func(cmd *cobra.Command, args []string) {
 		all, _ := cmd.Flags().GetBool("all")
 
-		statuses := []task.Status{task.PENDING}
+		statuses := []task.Status{task.PENDING, task.IN_PROGRESS}
 		if all {
-			statuses = append(statuses, task.IN_PROGRESS, task.PAUSED)
+			statuses = append(statuses, task.IN_PROGRESS, task.PAUSED, task.DONE)
 		}
 
 		dbConn, err := db.Connect()
